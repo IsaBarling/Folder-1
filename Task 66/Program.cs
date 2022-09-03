d78e2ebc8b92bdd1ee72ac5c3867a1c02075dd32
«Mary <isabarling@gmail.com>
2022-09-03 19:41:29 +0300

    version 2

--- Task 66/Program.cs
+++ Task 66/Program.cs
@@ -1,29 +1,62 @@
-﻿int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-int temp = m;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
 
-if (m > n) 
+namespace ConsoleApp1
 {
-  m = n; 
-  n = temp;
-}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte[] arr = File.ReadAllBytes("Numbers.exe");
 
-PrintSumm(m, n, temp=0);
+            //Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            //System.Reflection.Module manifestModule = executingAssembly.ManifestModule;
+            //System.Reflection.Module module = executingAssembly.LoadModule("ConsoleApp1.exe", arr);
 
-void PrintSumm(int m, int n, int summ)
-{
-  summ = summ + n;
-  if (n <= m)
-  {
-    Console.Write($"Сумма элементов= {summ} ");
-    return;
-  }
-  PrintSumm(m, n - 1, summ);
-}
+            ////AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(Resolve);
 
-int InputNumbers(string input) 
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
+            //MethodBase methodBase = module.ResolveMethod(0);
+
+            //methodBase.Invoke(null, null);
+
+            int m = InputNumbers("Введите m: ");
+            int n = InputNumbers("Введите n: ");
+
+            int temp = m;
+
+            if (m > n)
+            {
+                m = n;
+                n = temp;
+            }
+            //PrintSumm(m, n);
+            PrintSumm2(m, n);
+            Console.ReadLine();
+        }
+        
+        static void PrintSumm2(int m, int n)
+        {
+            int sum = 0;
+            for(int i = m + 1; i < n; i++)
+            {
+                if (i % 2 == 0)
+                    sum += i;
+
+            }
+            Console.Write("Sum is " + sum);
+
+        }
+
+        public static int InputNumbers(string input)
+        {
+            Console.Write(input);
+            int output = Convert.ToInt32(Console.ReadLine());
+            return output;
+        }
+    }
 }
