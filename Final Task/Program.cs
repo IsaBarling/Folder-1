89853176e0df89f59bf59a2db15d65821d71437f
«Mary <isabarling@gmail.com>
2022-09-14 20:40:43 +0300

    Step1

---
+++ Final Task/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FinalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Этап 1 Создание первичного массива для выборки
+             */
+            string[] array = GetUserArray();
+            
+            
+        }
+
+        
+
+        private static string[] GetUserArray()
+        {
+            Console.WriteLine("Введите элементы первого массива через запятую. Для завершения ввода нажмите Enter");
+            return Console.ReadLine().Split(',');
+        }
+    }
+}
